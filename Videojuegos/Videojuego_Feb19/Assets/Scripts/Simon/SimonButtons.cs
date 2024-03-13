b6452471c8a62b753b6da71ae46b0440540dc57b
Emily Rosenfeld <malkaheluani@MacBook-Pro-3.local>
2024-03-13 12:21:55 -0600

    Simon con Audio

--- Videojuegos/Videojuego_Feb19/Assets/Scripts/Simon/SimonButtons.cs
+++ Videojuegos/Videojuego_Feb19/Assets/Scripts/Simon/SimonButtons.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class SimonButtons : MonoBehaviour
 {
     Color originalColor;
+    AudioSource audioSource;
 
     void Start ()
     {
+        audioSource = GetComponent<AudioSource>();
         originalColor = GetComponent<Image>().color;
     }
+
     public void HighLight()
     {
         StartCoroutine(ChangeColor());
@@ -18,10 +21,16 @@ public class SimonButtons : MonoBehaviour
 
     IEnumerator ChangeColor()
     {
+        PlayAudio();
         GetComponent<Image>().color = new Color (0,0,0,0.5f);
         yield return new WaitForSeconds (0.5f);
         GetComponent<Image>().color = originalColor;
 
+    }
+
+    public void PlayAudio(){
+        Debug.Log("Button plaing: " + gameObject.name);
+        audioSource.Play();
     }
 
 }
